ff83c086ba053fbab28a4577fa7d96fda43cffe2
kesko <keskonmange@lalala.com>
2023-04-10 18:28:27 -0300

    First Version

---
+++ main.py
@@ -0,0 +1,62 @@
+
+def text_to_morse_code(text):
+    morse_code = {
+        "0": "-----",
+        "1": ".----",
+        "2": "..---",
+        "3": "...--",
+        "4": "....-",
+        "5": ".....",
+        "6": "-....",
+        "7": "--...",
+        "8": "---..",
+        "9": "----.",
+        "a": ".-",
+        "b": "-...",
+        "c": "-.-.",
+        "d": "-..",
+        "e": ".",
+        "f": "..-.",
+        "g": "--.",
+        "h": "....",
+        "i": "..",
+        "j": ".---",
+        "k": "-.-",
+        "l": ".-..",
+        "m": "--",
+        "n": "-.",
+        "o": "---",
+        "p": ".--.",
+        "q": "--.-",
+        "r": ".-.",
+        "s": "...",
+        "t": "-",
+        "u": "..-",
+        "v": "...-",
+        "w": ".--",
+        "x": "-..-",
+        "y": "-.--",
+        "z": "--..",
+        ".": ".-.-.-",
+        ",": "--..--",
+        "?": "..--..",
+        "!": "-.-.--",
+        "-": "-....-",
+        "/": "-..-.",
+        "@": ".--.-.",
+        "(": "-.--.",
+        ")": "-.--.-",
+        " ": " "
+    }
+    text = text.lower()
+    encoded_text = []
+    for letter in text:
+        encoded_text.append(morse_code[letter])
+    return ' '.join(encoded_text)
+
+
+print("\n*** WELCOME TO TEXT TO MORSE CODE CONVERTER! ***\n")
+msg = input(" - Please, type your message and press return: ")
+encoded_msg = text_to_morse_code(msg)
+
+print(f"\n -> Here's your encoded message: {encoded_msg}")
